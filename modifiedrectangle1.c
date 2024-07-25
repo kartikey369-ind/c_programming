59567f2c9a442cca1fad819ffd7583343ca4468e
kartikeyyy <kartikeykvk369@gmail.com>
2024-07-25 20:56:22 +0530

    Create modifiedrectangle1.c

---
+++ modifiedrectangle1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int length, breadth ;
+    int i ; char;
+
+    printf("enter the length : ");
+    scanf("%d",&length);
+    if(length == char)
+    {
+        printf("enter integer only");
+    }
+    
+    else 
+    {
+    printf("enter the breadth : ");
+    scanf("%d",&breadth);
+    }
+    
+    if(breadth == char)
+    {
+        printf("enter integer only");
+    }
+
+    else
+   {
+     printf("the area of rectangle is : %d\n", length * breadth);
+   }
+}
