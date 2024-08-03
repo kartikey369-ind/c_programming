82df8d96f012dd0265c3e259b54de30ca605252d
kartikzz <kartikeykvk369@gmail.com>
2024-08-03 16:11:57 +0000

    Sat, Aug 3, 2024, 9:41 PM +05:30

---
+++ pass.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a ;
+    printf("enter score",a);
+    scanf("%d",&a);
+
+
+
+
+    if(a >= 90 &&  a <= 100)
+    {
+        printf("grade A");
+    }
+
+    else if(a >= 80 && a < 90 )
+    {
+                printf("grade B");
+
+    }
+
+    
+
+
+
+
+
+
+
+}
