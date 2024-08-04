d34037c7775e89e92b8db88f6059aca8e1168dcb
kartikzz <kartikeykvk369@gmail.com>
2024-08-04 10:33:29 +0000

    Sun, Aug 4, 2024, 4:03 PM +05:30

--- guess.c
+++ guess.c
@@ -6,8 +6,9 @@ int main() {
     // Seed the random number generator with the current time
     srand(time(0));
     // Generate a random number between 1 and 100
-    int randomNumber = rand() % 100 + 1;
-    
+    int randomNumber = rand() % 100 + 1 ;
+    int no_of_guesses = 0 ;
+    int guessed ;
 
     int i;
     printf("ENTER YOUR GUESS : ");
