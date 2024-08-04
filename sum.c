bc404364a89aa2d3ac5892f2f6ccf4f739c8b1a8
kartikzz <kartikeykvk369@gmail.com>
2024-08-04 15:36:02 +0000

    Sun, Aug 4, 2024, 9:06 PM +05:30

---
+++ sum.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int sum(int,int);
+int average(int,int);
+
+
+int main(void)
+{
+    int a = 1 , b = 2;
+    printf()
+
+
+}
+int sum(int x,int y)
+{
+    sum = a + b ;
+}
+
+int average(int,int)
+{
+    average = (a + b)/2 ;
+}
+
+
