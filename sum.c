4a5cbdf6c0cb933588bda830bc5eabf21b2ac4b5
kartikzz <kartikeykvk369@gmail.com>
2024-08-04 15:43:12 +0000

    Sun, Aug 4, 2024, 9:13 PM +05:30

--- sum.c
+++ sum.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 
 int sum(int,int);
-int average(int,int);
+float average(int,int);
 
 
 int main(void)
 {
     int a = 1 , b = 2;
-
+    printf("the sum is %d\n",sum(a, b));
+    printf("the average is %f\n",average(a, b));
 
 
 }
+
 int sum(int a,int b)
 {
-    printf("the sum is %d\n ",a + b);
+
+    return a + b;
 }
 
 float average(int a,int b)
 {
-   printf("the sum is %f\n ",(a + b)/2.0);
+   
+   return (a + b)/2.0 ;
 }
 
 
