59710619cc087f5277279d40360d4a83d7d1c7b6
kartikzz <kartikeykvk369@gmail.com>
2024-08-04 09:27:21 +0000

    Sun, Aug 4, 2024, 2:57 PM +05:30

---
+++ multiply.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n ;
+    printf("enter n %d \n",n);
+    scanf("%d",&n);
+
+    for(int i = 0 ; i < 10 ; i++ )
+    {
+        printf("the multiplication table is : \n",n*i);
+    }
+
+}
