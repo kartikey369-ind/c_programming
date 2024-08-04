4b034382b9dbb3e6ee0a78d47079d7ba6a09d781
kartikzz <kartikeykvk369@gmail.com>
2024-08-04 11:37:58 +0000

    Sun, Aug 4, 2024, 5:07 PM +05:30

---
+++ average.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+float average(float,float,float);
+
+
+int main(void)
+{
+
+        printf("the average is %f\n",average);
+
+
+}
+
+
+float average(float,float,float)
+{
+    float a = 1, b = 2 , c = 3 ;
+    float average;
+    average = (a + b + c)/3 ;
+
+}
+
