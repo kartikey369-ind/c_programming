2c36b8491d5eead39911ae34a9eb8d3cc71cb03a
kartikzz <kartikeykvk369@gmail.com>
2024-08-04 15:22:45 +0000

    Sun, Aug 4, 2024, 8:52 PM +05:30

--- pointer.c
+++ pointer.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
+int func(int,int);
+
 int main(void)
 {
-     i = 3;
+    int a = 3;
 
 
-    printf("the adress of i is %u\n",&i);
+    printf("the adress of i is %p\n",&a);
+}
+
+int func(int,int)
+{
+    int* b = &a;
+    printf("the adress of b is %p\n",&b)
 }
