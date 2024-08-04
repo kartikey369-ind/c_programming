dc94ef1ffd29fa19c2f80528555ab1feb6f4c84b
kartikzz <kartikeykvk369@gmail.com>
2024-08-04 11:43:36 +0000

    Sun, Aug 4, 2024, 5:13 PM +05:30

--- average.c
+++ average.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 
-float average(float,float,float);
+float average(int a ,  int b, int c );
+float average(int a,  int b, int c)
+{
+
+    int average;
+    return (a + b + c)/3 ;
+
+
+}
 
 
 int main(void)
 {
+        int a = 1, b = 2 , c = 3 ;
 
-        printf("the average is %f\n",average);
+        printf("the average is %f\n", average(a,b,c ));
 
 
 }
 
 
-float average(float,float,float)
-{
-    float a = 1, b = 2 , c = 3 ;
-    float average;
-    average = (a + b + c)/3 ;
 
-}
 
